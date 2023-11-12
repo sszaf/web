835c87aaf978784296b4ee355dd0391e20cc8a1a
sszaf <266605@student.pwr.edu.pl>
2023-11-12 14:00:20 +0100

    js first commit

---
+++ scripts.js
@@ -0,0 +1,38 @@
+const attractionInsertInput = document.getElementById("attractionInsertInput");
+const attractionList = document.getElementById("attractionList");
+const attractionInsertButton = document.getElementById("attractionInsertButton");
+
+attractionInsertButton.addEventListener("click",insertAttraction);
+
+function insertAttraction()
+{
+    const inputContent = attractionInsertInput.value;
+    
+    if(inputContent.length != 0)
+    {
+        const existingData = JSON.parse(localStorage.getItem("userAttractions")) || [];
+        existingData.push(inputContent);
+        localStorage.setItem("userAttractions", JSON.stringify(existingData));
+
+
+        displayAttractions();
+        window.alert("Atrakcja została dodana do listy.");
+    }
+    else
+    {
+        window.alert("Nie wprowadzono zmian!");
+    }
+
+}
+
+
+function displayAttractions()
+{
+    const savedAttractions = JSON.parse(localStorage.getItem("userAttractions")) || [];
+
+    savedAttractions.forEach(function(item){
+        attractionList.innerHTML += "<li>" + item + "</li>";
+    })
+}
+
+window.onload = displayAttractions;
