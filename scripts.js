4f1531ea7e480a645e0c88b1f1b1310c470d6fd3
sszaf <266605@student.pwr.edu.pl>
2023-11-13 12:25:07 +0100

    validation update

--- scripts.js
+++ scripts.js
@@ -1,38 +1,36 @@
 const attractionInsertInput = document.getElementById("attractionInsertInput");
 const attractionList = document.getElementById("attractionList");
-const attractionInsertButton = document.getElementById("attractionInsertButton");
+const attractionInsertButton = document.getElementById(
+    "attractionInsertButton"
+);
 
-attractionInsertButton.addEventListener("click",insertAttraction);
+attractionInsertButton.addEventListener("click", insertAttraction);
 
-function insertAttraction()
-{
+function insertAttraction() {
     const inputContent = attractionInsertInput.value;
-    
-    if(inputContent.length != 0)
-    {
-        const existingData = JSON.parse(localStorage.getItem("userAttractions")) || [];
+    if (inputContent.length !== 0) {
+        const existingData = JSON.parse(
+            localStorage.getItem("userAttractions")
+        ) || [];
         existingData.push(inputContent);
         localStorage.setItem("userAttractions", JSON.stringify(existingData));
-
-
         displayAttractions();
         window.alert("Atrakcja została dodana do listy.");
-    }
-    else
-    {
+    } else {
         window.alert("Nie wprowadzono zmian!");
     }
 
 }
 
 
-function displayAttractions()
-{
-    const savedAttractions = JSON.parse(localStorage.getItem("userAttractions")) || [];
+function displayAttractions() {
+    const savedAttractions = JSON.parse(
+        localStorage.getItem("userAttractions")
+    ) || [];
 
-    savedAttractions.forEach(function(item){
+    savedAttractions.forEach(function (item) {
         attractionList.innerHTML += "<li>" + item + "</li>";
-    })
+    });
 }
 
 window.onload = displayAttractions;
