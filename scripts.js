3f1fe98c14354f958a3d6601a6bb1560d40b4a59
sszaf <266605@student.pwr.edu.pl>
2023-11-13 16:05:48 +0100

    js structure correction

--- scripts.js
+++ scripts.js
@@ -1,36 +1,43 @@
-const attractionInsertInput = document.getElementById("attractionInsertInput");
-const attractionList = document.getElementById("attractionList");
-const attractionInsertButton = document.getElementById(
-    "attractionInsertButton"
-);
+var currentPage = window.location.pathname;
 
-attractionInsertButton.addEventListener("click", insertAttraction);
+if (currentPage.endsWith("Atrakcje.html")) {
 
-function insertAttraction() {
-    const inputContent = attractionInsertInput.value;
-    if (inputContent.length !== 0) {
-        const existingData = JSON.parse(
+    const attractionInsertInput = document.getElementById("attractionInsertInput");
+    const attractionList = document.getElementById("attractionList");
+    const attractionInsertButton = document.getElementById(
+        "attractionInsertButton"
+        );
+    
+    window.addEventListener("load",displayAttractions);
+    
+    attractionInsertButton.addEventListener("click", insertAttraction);
+    
+    function insertAttraction() {
+        const inputContent = attractionInsertInput.value;
+        if (inputContent.length !== 0) {
+            const existingData = JSON.parse(
+                localStorage.getItem("userAttractions")
+            ) || [];
+            existingData.push(inputContent);
+            localStorage.setItem("userAttractions", JSON.stringify(existingData));
+            displayAttractions();
+            window.alert("Atrakcja została dodana do listy.");
+        } else {
+            window.alert("Nie wprowadzono zmian!");
+        }
+    
+    }
+    
+    
+    function displayAttractions() {
+        const savedAttractions = JSON.parse(
             localStorage.getItem("userAttractions")
         ) || [];
-        existingData.push(inputContent);
-        localStorage.setItem("userAttractions", JSON.stringify(existingData));
-        displayAttractions();
-        window.alert("Atrakcja została dodana do listy.");
-    } else {
-        window.alert("Nie wprowadzono zmian!");
+    
+        attractionList.innerHTML = "";
+        savedAttractions.forEach(function (item) {
+            attractionList.innerHTML += "<li>" + item + "</li>";
+        });
     }
-
 }
 
-
-function displayAttractions() {
-    const savedAttractions = JSON.parse(
-        localStorage.getItem("userAttractions")
-    ) || [];
-
-    savedAttractions.forEach(function (item) {
-        attractionList.innerHTML += "<li>" + item + "</li>";
-    });
-}
-
-window.onload = displayAttractions;
